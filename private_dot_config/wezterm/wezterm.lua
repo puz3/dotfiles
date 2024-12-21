6e646e172b95ac050b3d6912c0544e2b7a6e23fe
Pūzė <povilas.puz@gmail.com>
2024-12-21 13:27:40 +0200

    Remove unused font in the Wezterm config file

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Mocha'
 
 config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
--- config.font = wezterm.font 'MonaspiceNe Nerd Font Mono'
 config.font_size = 16
 
 config.hide_tab_bar_if_only_one_tab = true
