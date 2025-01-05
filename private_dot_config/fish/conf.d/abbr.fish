7f058169391ad69d5166607c6f6f91bb7d66e34c
Pūzė <povilas.puz@gmail.com>
2025-01-05 16:36:57 +0200

    Update Fish brew abbreviations

--- private_dot_config/fish/conf.d/abbr.fish
+++ private_dot_config/fish/conf.d/abbr.fish
@@ -1,6 +1,6 @@
 # Alias some of the brew commands
-abbr --add bu "brew update;brew upgrade"
-abbr --add bc "brew autoremove;brew cleanup"
+abbr --add bu "brew update && brew upgrade"
+abbr --add bc "brew autoremove && brew cleanup"
 
 # Alias to use eza if it installed on the system.
 if type -q eza
@@ -9,7 +9,7 @@ if type -q eza
   abbr --add la 'eza -lagh --git --icons --group-directories-first --sort modified'
   abbr --add cll 'clear; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first'
 
-## Tree view
+  # Tree view
   abbr --add tree 'eza -Ta --icons --ignore-glob="node_modules|.git|.vscode|.DS_Store"'
   abbr --add ltd 'eza -TaD --icons --ignore-glob="node_modules|.git|.vscode|.DS_Store"'
 else
