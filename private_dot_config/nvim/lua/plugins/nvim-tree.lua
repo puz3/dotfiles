a057335cb9235ed99a9b86b071672a7c166050ae
Pūzė <povilas.puz@gmail.com>
2025-04-27 16:25:17 +0300

    Add keybinds for nvim-tree

--- private_dot_config/nvim/lua/plugins/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,11 @@ return {
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
+		keys = {
+			{ "<leader>tt", "<cmd>NvimTreeToggle<cr>", desc = "Toggle nvim-tree" },
+			{ "<leader>tF", "<cmd>NvimTreeFocus<cr>", desc = "Focus nvim-tree" },
+			{ "<leader>tf", "<cmd>NvimTreeFindFile<cr>", desc = "Find in nvim-tree" },
+		},
 		config = function()
 			require("nvim-tree").setup({})
 		end,
