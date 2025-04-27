b7b1ef44df2638bf6c7c9f38aa45ef48e7529042
Pūzė <povilas.puz@gmail.com>
2025-04-27 16:18:56 +0300

    Format nvim-skim

--- private_dot_config/nvim/lua/plugins/skim.lua
+++ private_dot_config/nvim/lua/plugins/skim.lua
@@ -1,5 +1,7 @@
 return {
-	"ibhagwan/fzf-lua",
-	-- enable `sk` support instead of the default `fzf`
-	opts = { "skim" },
+	{
+		"ibhagwan/fzf-lua",
+		-- enable `sk` support instead of the default `fzf`
+		opts = { "skim" },
+	},
 }
